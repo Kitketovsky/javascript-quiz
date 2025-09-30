f6a39d3e7a924930ee37a8e51ad7470284bbbbee
Kitketovsky <kitketovsky@gmail.com>
2025-09-30 17:40:51 +0400

    CodeViewer component

---
+++ src/components/CodeViewer.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github.css'
+
+defineProps<{
+  code: string
+}>()
+</script>
+
+<!-- TODO: move all replace logic to parsing -->
+
+<template>
+  <pre>
+          <code class="hljs" v-html="hljs.highlight(code
+              .replace(/```javascript/, '')
+              .replace(/```/, '')
+              .replace(/\n/, ''), { language: 'javascript' }).value"></code>
+         </pre>
+</template>
+
+<style scoped>
+pre {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem 0;
+  font-size: 1.1rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.3rem;
+  }
+}
+
+.hljs {
+  overflow-x: auto;
+  background-color: transparent;
+  padding: 0;
+}
+</style>
